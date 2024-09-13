1ba67c55e2affdfc1cd23733dfb983c68a19d1fe
Admin <Admin@iSokalezwe>
2024-09-14 01:34:15 +0200

    Initial commit add comment

---
+++ Vowel-Count/src/main/java/com/mycompany/vowel/count/VowelCount.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vowel.count;
+
+/**
+ *
+ * @author Admin
+ */
+public class VowelCount {
+
+    public static void main(String[] args) {
+        System.out.println("Sawubona Nkosazana, unjani?");
+    }
+}
