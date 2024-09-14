17636719d794e1bd73fc7134317c0cd8eacb1ea4
Admin <Admin@iSokalezwe>
2024-09-14 02:21:35 +0200

    Initial commit Add comment Improve formatting

--- Vowel-Count/src/main/java/com/mycompany/vowel/count/VowelCount.java
+++ Vowel-Count/src/main/java/com/mycompany/vowel/count/VowelCount.java
@@ -8,9 +8,33 @@ package com.mycompany.vowel.count;
  *
  * @author Admin
  */
+import java.util.Scanner;
+
 public class VowelCount {
+    public static int countVowels(String sentence) {
+        int vowelCount = 0;
+        sentence = sentence.toLowerCase();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char currentChar = sentence.charAt(i);
+            if (isVowel(currentChar)) {
+                vowelCount++;
+            }
+        }
+
+        return vowelCount;
+    }
+
+    private static boolean isVowel(char ch) {
+        return "aeiou".indexOf(ch) != -1;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Sawubona Nkosazana, unjani?");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+
+        int vowelCount = countVowels(sentence);
+        System.out.println("Number of vowels: " + vowelCount);
     }
 }
