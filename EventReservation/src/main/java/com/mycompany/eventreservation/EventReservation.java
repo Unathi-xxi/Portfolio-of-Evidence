7ce14327a0259c248d463ba9dbd90b198f7fb6bb
Admin <Admin@iSokalezwe>
2024-10-04 22:51:06 +0200

    Initial commit Add comment Improve formatting

---
+++ EventReservation/src/main/java/com/mycompany/eventreservation/EventReservation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eventreservation;
+
+/**
+ *
+ * @author Admin
+ */
+public class EventReservation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
