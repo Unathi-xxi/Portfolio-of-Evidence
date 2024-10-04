eeda7b7bd5e867c8e181e4071022ce3fa4c9841b
Admin <Admin@iSokalezwe>
2024-10-04 22:27:52 +0200

    Initial commit Add comment Improve formatting

---
+++ EventReservationSystemTest/src/main/java/com/mycompany/eventreservationsystemtest/EventReservationSystemTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eventreservationsystemtest;
+
+/**
+ *
+ * @author Admin
+ */
+public class EventReservationSystemTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
